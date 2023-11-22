dad4c4ee17f8fef3f76d0fd9e9de608a793c35f1
zachpwakefield <zachpwakefield@gmail.com>
2023-11-22 12:48:45 -0500

    trying to reset

--- R/lfc.R
+++ R/lfc.R
@@ -1,25 +1,24 @@
 lfc <- function(de_df, numCont, numExp, exon_type, cores = 8) {
   de_df <- de_df[de_df$type == exon_type,]
   samps <- colnames(de_df)
-  # unlist(lapply(strsplit(colnames(de_df)[-c(1:10, (10+numCont+numExp+1):dim(de_df)[2])], split = "[.]"), "[[", 5))
-  
+
   lfc <- list()
   col <- list()
-  
-  
+
+
   lfc <- mclapply(1:length(de_df$gene), mc.cores = cores, function(i) {
     outlier <- which(unlist(lapply(samps, function(x) grepl(x, de_df$outlier[i]))))+10
-    
+
     cont <- c(11:(11-1+numCont))
     cont <- cont[!(cont %in% outlier)]
     exp <- c((numCont+10+1):(11-1+numCont+numExp))
     exp <- exp[!(exp %in% outlier)]
-    
-    
-    
+
+
+
     if (length(exp) == 0) {
       lfc_t <- log2((.01)/as.numeric(rowMeans(de_df[i,cont, drop = FALSE])+.01))
-    } 
+    }
     if (length(cont) == 0) {
       lfc_t <- log2((as.numeric(rowMeans(de_df[i,exp, drop = FALSE]))+.01)/.01)
     }
@@ -29,11 +28,11 @@ lfc <- function(de_df, numCont, numExp, exon_type, cores = 8) {
     if (length(exp) != 0 & length(cont) != 0) {
       lfc_t <- log((as.numeric(rowMeans(de_df[i,exp, drop = FALSE]))+.01)/as.numeric(rowMeans(de_df[i,cont, drop = FALSE])+.01))
     }
-    
-    
+
+
     lfc_t
   })
-  
+
   de_df$lfc <- unlist(lfc)
   de_df <- de_df[de_df$p_value >= 0,]
   col <- list()
@@ -45,10 +44,10 @@ lfc <- function(de_df, numCont, numExp, exon_type, cores = 8) {
     if (de_df$lfc[i] >= 1.0 & de_df$p_value[i] < .01) {
       col[[i]] <- "#00A79D"# natparks.pals("Acadia", 15)[1]
     }
-    
+
   }
   de_df$col <- unlist(col)
-  
+
   p.scDE <- de_df
   p.scDE$numOutliers <- str_count(p.scDE$outlier, "[.]")
   return (de = p.scDE)
