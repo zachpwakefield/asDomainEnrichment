343e62a9cae241bf26aa9dab68638d6fa6cae285
zachpwakefield <zachpwakefield@gmail.com>
2023-11-27 10:50:06 -0500

    added system call

--- R/run_interproscan.R
+++ R/run_interproscan.R
@@ -1,17 +1,17 @@
-run_interproscan <- function(bash_location, interproscan_location, cluster = T, project, memory, name_job, email, output_location) {
+run_interproscan <- function(bash_location, interproscan_location, cluster = F, project, memory, name_job, email, output_location) {
   if (cluster) {
-    paste("qsub ", bash_location, "run_cluster_interproscan.sh",
+    system(paste("qsub ", bash_location, "run_cluster_interproscan.sh",
           " ", project,
           " ", memory,
           " ", name_job,
           " ", email,
           " ", output_location,
-          " ", interproscan_location, sep="")
+          " ", interproscan_location, sep=""))
   } else {
-    paste("bash ", bash_location, "run_interproscan.sh",
+    system(paste("bash ", bash_location, "run_interproscan.sh",
           " ", output_location,
           " ", interproscan_location,
-          sep="")
+          sep=""))
   }
 
 }
