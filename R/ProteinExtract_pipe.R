5988e9254de818b16f9d834090f79edce86931ed
zachpwakefield <zachpwakefield@gmail.com>
2023-11-28 17:50:05 -0500

    delta psi

--- R/ProteinExtract_pipe.R
+++ R/ProteinExtract_pipe.R
@@ -1,4 +1,4 @@
-proteinExtract_pipe <- function(files_dir, background = T, updown = c('up', 'down')[1], lfc_thresh = 1, fdr = .05, mOverlap = .5,
+proteinExtract_pipe <- function(files_dir, background = T, updown = c('up', 'down')[1], thresh = .4, fdr = .05, mOverlap = .5,
                                 saveOutput = F, inCores = 8, nC = 0, nE = 0, exon_type = "AFE",
                                 location = system.file(package="domainEnrichment"), output_location) {
 
@@ -19,9 +19,9 @@ proteinExtract_pipe <- function(files_dir, background = T, updown = c('up', 'dow
     df.l <- lfc(df, numCont = nC, numExp = nE, exon_type = exon_type, cores = inCores)
     lfcPlot <- make_lfcPlot(df.l)
     if (updown == "up") {
-      cdf.l <- df.l[df.l$lfc >= lfc_thresh & df.l$p_value <= fdr,]
+      cdf.l <- df.l[df.l$delta_PSI >= thresh & df.l$p_value <= fdr,]
     } else {
-      cdf.l <- df.l[df.l$lfc <= -(lfc_thresh) & df.l$p_value <= fdr,]
+      cdf.l <- df.l[df.l$delta_PSI <= -(thresh) & df.l$p_value <= fdr,]
     }
     redExon <- data.frame(geneR = unlist(lapply(strsplit(cdf.l$gene, split = "[.]"), "[[", 1)),
                           chr = sapply(strsplit(cdf.l$exon, split = ":"), "[[", 1),
