fc5857e013565d0be32a812e1d9dc549797cb60f
zachpwakefield <zachpwakefield@gmail.com>
2023-11-29 17:47:43 -0500

    eh

--- R/setup.R
+++ R/setup.R
@@ -7,8 +7,8 @@ get_c_trans <- function(location) {
 }
 
 # get_gtf
-# input: directory containing gtf_withInfo, default should be package directory or: system.file(package="domainEnrichment")
-# output: gtf_withInfo.csv csv
+# input: directory containing all the gtf files, default should be package directory or: system.file(package="domainEnrichment")
+# output: cg csv
 
 get_gtf <- function(location) {
   cg <- do.call(rbind, lapply(paste0(location, paste0(c("first", paste0("internal", 1:5), "last"), "_gtf.csv")), function(x) {
